3e1b19ca6a9e843657f2180093d550a54e1f0595
Kumar Ambuj <kumarambuj1997@gmail.com>
2023-05-17 22:46:50 +0530

    variables

--- tutorial.js
+++ tutorial.js
@@ -1,1 +1,24 @@
-console.log("shri ganeshay namah");
+
+console.log("dekh bhai aaj javascript padhenge");
+var fname="Ambuj";
+var lname="Shekhar";
+console.log(fname);
+console.log(fname,lname);
+console.log(typeof fname);
+
+var a="hello";
+console.log(a,typeof a);
+a=123;
+console.log(a,typeof a);
+
+console.log(window);
+console.log(window.a);
+
+b=30;
+console.log(window.b);
+
+var c=40.345;
+console.log(c,typeof c);
+
+console.log(typeof Infinity);
+console.log(typeof -Infinity);
